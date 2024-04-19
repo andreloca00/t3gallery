8371d0a16db4779c4ca8e29d8615ee918244fa49
Andrea Locatelli <andrealocatelli2000@gmail.com>
2024-04-19 21:24:13 +0200

    Scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const imgUrls = [
+  "https://utfs.io/f/6c954832-7520-4b56-85a3-8780bba73126-tpcmqd.jpg",
+  "https://utfs.io/f/121fc93b-832e-4d35-b460-6746408cd97a-tha4rm.jpg",
+  "https://utfs.io/f/8196b95c-f315-40f1-a15f-1cd36d11d8d1-jxogc5.jpg",
+  "https://utfs.io/f/439392a1-be9c-4fa8-8a71-87d9a2fab315-za1hmz.jpg",
+];
+
+const mockImages = imgUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>hello gallery in progress</div>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="" className="h-auto w-full" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
