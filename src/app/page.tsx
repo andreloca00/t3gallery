7afb756c8e13152ee8b9c5233563cc4e8beb5945
Andrea Locatelli <andrealocatelli2000@gmail.com>
2024-04-21 20:01:08 +0200

    Add auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-const imgUrls = [
-  "https://utfs.io/f/6c954832-7520-4b56-85a3-8780bba73126-tpcmqd.jpg",
-  "https://utfs.io/f/121fc93b-832e-4d35-b460-6746408cd97a-tha4rm.jpg",
-  "https://utfs.io/f/8196b95c-f315-40f1-a15f-1cd36d11d8d1-jxogc5.jpg",
-  "https://utfs.io/f/439392a1-be9c-4fa8-8a71-87d9a2fab315-za1hmz.jpg",
-];
+export const dynamic = "force-dynamic";
 
-const mockImages = imgUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
-export default function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...images, ...images, ...images].map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
             <img src={image.url} alt="" className="h-auto w-full" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
