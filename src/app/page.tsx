9741eb4b8e56dbae5ccb31c9e8e033d78de79ae5
Andrea Locatelli <andrealocatelli2000@gmail.com>
2024-04-21 21:38:26 +0200

    added upload button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
+import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} alt="" className="h-auto w-full" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} alt="" className="h-auto w-full" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please log in to view images!
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
