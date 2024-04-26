63a795aac015398a07c0d4c58a73b0caad29f308
Andrea Locatelli <andrealocatelli2000@gmail.com>
2024-04-26 19:01:57 +0200

    fixed layout

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,14 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} alt={image.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink">
+        <img src={image.url} alt={image.name} className="w-96 object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col">
+        <h2 className="text-xl font-bold">{image.name}</h2>
+      </div>
+    </div>
+  );
 }
